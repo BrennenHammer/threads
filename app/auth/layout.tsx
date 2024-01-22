0542cd695bfdbb71183132fe59733e5e10d0844a
Test <Hammertyme101@gmail.com>
2024-01-22 15:38:02 -0500

    check1

---
+++ app/auth/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+    title: 'threads',
+    description: 'A next.js 13 Meta Threads Application'
+}
+
+export default function RootLayout({
+     children }:
+      { children: React.ReactNode})
+ {
+        return
+    }
